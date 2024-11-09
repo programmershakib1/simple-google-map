ccb33a22f72ab1f7969b6dc246f9ccc292ce776c
programmershakib <programmershakibinfo@gmail.com>
2024-11-09 14:39:00 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { APIProvider } from '@vis.gl/react-google-maps'
+import { googleMapAPIKey } from './map-api-key.js'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <APIProvider apiKey={googleMapAPIKey}>
+      <App></App>
+    </APIProvider>
+  </StrictMode>,
+)
